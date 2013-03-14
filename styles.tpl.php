dd72385d090c9d6a67a5108954e535c5766e5575
James Reidy <jgr25@cornell.edu>
2013-03-14 18:19:28 +0000

    switch to js file
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@18 7fd527ee-9837-423d-921d-0a34619f9d71

--- styles.tpl.php
+++ styles.tpl.php
@@ -18,7 +18,8 @@
 <?php print $prefix; ?>
 <?php
 if ($style_name == 'tilezoom') {
-  $arr = file_stream_wrapper_get_instance_by_uri($variables['object']->uri)->getDirectoryPath();
+  $uri = $variables['object']->uri;
+  $arr = file_stream_wrapper_get_instance_by_uri($variables['object']->uri)->realpath();
   print '<pre>';
   print_r($arr);
   print '</pre>';
@@ -31,6 +32,14 @@ if ($style_name == 'tilezoom') {
   // add the javascript to support tilezoom
   drupal_add_js(drupal_get_path('theme', 'warburg') . '/js/jquery.mousewheel.js', array('group' => JS_THEME));
   drupal_add_js(drupal_get_path('theme', 'warburg') . '/js/tilezoom/jquery.tilezoom.js', array('group' => JS_THEME));
+
+  // inline js code for the ready function
+  $width = 2835;
+  $height = 4289;
+  $path = "../drupal_files/panels/Panel C_files";
+  $tilezoom = "$('#tilezoom-container').tilezoom({width: $width, height: $height, path: '$path', mousewheel: true});";
+  $startposition = "$('#tilezoom-starthere').click();"
+  $ready = "$(document).ready(function(){ $tilezoom $startposition });"
 }
 else {
   print $output;
