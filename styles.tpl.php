0bfb720b448daa80da3318f2e0d5d0f79fa733bf
James Reidy <jgr25@cornell.edu>
2013-03-14 16:24:59 +0000

    call it a file
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@11 7fd527ee-9837-423d-921d-0a34619f9d71

--- styles.tpl.php
+++ styles.tpl.php
@@ -26,7 +26,7 @@ if ($style_name == 'tilezoom') {
   //$url = $varibales['object']->uri;
 
   // add the css container class definition for #tilezoom-container
-  drupal_add_css(drupal_get_path('theme', 'warburg') . '/css/tilezoom.css', options);
+  drupal_add_css(drupal_get_path('theme', 'warburg') . '/css/tilezoom.css', array('type' => 'file'));
 
   // add the javascript to support tilezoom
   drupal_add_js(drupal_get_path('theme', 'warburg') . '/js/jquery.mousewheel.js', array('group' => JS_THEME));
