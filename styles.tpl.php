70da001b66299be4204f5c95236396f9cd490a25
James Reidy <jgr25@cornell.edu>
2013-03-14 16:22:41 +0000

    import css and javascript for tilezoom
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@10 7fd527ee-9837-423d-921d-0a34619f9d71

--- styles.tpl.php
+++ styles.tpl.php
@@ -24,6 +24,13 @@ if ($style_name == 'tilezoom') {
   print '</pre>';
 
   //$url = $varibales['object']->uri;
+
+  // add the css container class definition for #tilezoom-container
+  drupal_add_css(drupal_get_path('theme', 'warburg') . '/css/tilezoom.css', options);
+
+  // add the javascript to support tilezoom
+  drupal_add_js(drupal_get_path('theme', 'warburg') . '/js/jquery.mousewheel.js', array('group' => JS_THEME));
+  drupal_add_js(drupal_get_path('theme', 'warburg') . '/js/tilezoom/jquery.tilezoom.js', array('group' => JS_THEME));
 }
 else {
   print $output;
