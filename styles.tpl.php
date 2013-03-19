1cf7a9540bf89039bb054839bc760c3d63c3daa1
James Reidy <jgr25@cornell.edu>
2013-03-19 20:46:44 +0000

    forgot $
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@96 7fd527ee-9837-423d-921d-0a34619f9d71

--- styles.tpl.php
+++ styles.tpl.php
@@ -178,7 +178,7 @@ if ($style_name == 'tilezoom') {
         $seq = node_load($seq_id);
         if ($seq !== FALSE) {
           if (isset($seq->field_steps['und'])) {
-            prefix = '';
+            $prefix = '';
             foreach ($seq->field_steps['und'] as $val) {
               $hotspot = warburg_hotspot_list($val['target_id']);
               $hotspots[] = warburg_hotspot_format($hotspot, $prefix . $hotspot['type']);
