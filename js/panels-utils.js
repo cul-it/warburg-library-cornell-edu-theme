821f63b8ba4015138b815142dd7bc3ffc2894f3b
Manolo Bevia <mb863@cornell.edu>
2013-05-01 20:04:29 +0000

    Theming pannels.
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@150 7fd527ee-9837-423d-921d-0a34619f9d71

---
+++ js/panels-utils.js
@@ -0,0 +1,93 @@
+
+
+//Comment? get max height for panels
+
+var $header = $('.panels header');
+
+var headerHeight = $header.height() + parseInt($header.css("paddingTop") + $header.css("paddingBottom") + $header.css("marginTop") + $header.css("marginBottom"));
+
+var $carousel = $('.rs-carousel');
+
+var carouselHeight = $carousel.height() + parseInt($carousel.css("paddingTop") + $carousel.css("paddingBottom") + $carousel.css("marginTop") + $carousel.css("marginBottom"));
+
+var $winHeight = $(window).height();
+
+var maxHeightPanel = $winHeight - ((headerHeight + carouselHeight) + 10);
+  
+$('.panels-display').height(maxHeightPanel); 
+
+$('.panels-photo').height(maxHeightPanel);
+$('.panel-description').height(maxHeightPanel);
+$('.panels-display nav').height(maxHeightPanel);
+
+
+
+
+ 
+//console.log($('.win').html($winHeight));   
+          
+$(window).resize(function(){
+    
+    headerHeight = $header.height() + parseInt($header.css("paddingTop") + $header.css("paddingBottom") + $header.css("marginTop") + $header.css("marginBottom"));
+    
+    carouselHeight = $carousel.height() + parseInt($carousel.css("paddingTop") + $carousel.css("paddingBottom") + $carousel.css("marginTop") + $carousel.css("marginBottom"));
+
+    $winHeight = ($(window).height()) + 10;
+
+    maxHeightPanel = $winHeight - ((headerHeight + carouselHeight) + 10);
+    
+    $('.panels-display').height(maxHeightPanel);  
+    $('.panels-photo').height(maxHeightPanel);
+    $('.panel-description').height(maxHeightPanel);
+    $('.panels-display nav').height(maxHeightPanel);
+    
+    
+    //center panel
+    //$('#container').hide();
+    
+    $(':rs-carousel').carousel('refresh');
+    console.log( maxHeightPanel + " Refreshed");
+});
+
+
+
+$(document).ready(function () {
+    $('.rs-carousel').carousel(
+        {
+            nextPrevActions: true,
+            insertPrevAction: function () {
+                return $('<a href="#" class="rs-carousel-action rs-carousel-action-prev"><span class="carousel-left-control"></span></a>').appendTo(this);
+            },
+            insertNextAction: function () {
+                return $('<a href="#" class="rs-carousel-action rs-carousel-action-next"><span class="carousel-right-control"></span></a>').appendTo(this);
+            },
+            pagination: false
+        }
+    );
+    
+    
+    //ZOOM
+    $('#container').tilezoom({
+			xml: 'dest/Panel79a.xml',
+			mousewheel: true,
+			navigation: true,
+			zoomIn: '#plus',
+			zoomOut: '#minus',
+			goHome: '#home',
+			navigation: '#panel-tools li.display-tools'
+			//'#panel-tools'
+
+		});
+		
+		
+		//hover over carousel of panels will change image legend to active color.
+		$('.panel-item').hover(function(){
+		  $(this).find('.panel-name').addClass('hover');
+		}, function(){
+		  $(this).find('.panel-name').removeClass('hover');
+		});
+
+});
+
+
+
