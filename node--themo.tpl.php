4220e7ef3903eb1810a4650e71696b2bf8b616d8
James Reidy <jgr25@cornell.edu>
2013-05-01 15:40:36 +0000

    renamed for drupal 7
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@128 7fd527ee-9837-423d-921d-0a34619f9d71

---
+++ node--themo.tpl.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * node-themo.tpl.phh
+ * copy of Garland's node.tpl.php used to render content type with internal name 'themo'
+ */
+?>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <?php print $user_picture; ?>
+
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <?php if ($display_submitted): ?>
+    <span class="submitted"><?php print $submitted ?></span>
+  <?php endif; ?>
+
+  <div class="content clearfix"<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      <h2>Themo!</h2>
+      print render($content);
+    ?>
+  </div>
+
+  <div class="clearfix">
+    <?php if (!empty($content['links'])): ?>
+      <div class="links"><?php print render($content['links']); ?></div>
+    <?php endif; ?>
+
+    <?php print render($content['comments']); ?>
+  </div>
+
+</div>
