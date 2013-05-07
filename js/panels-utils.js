b238acf37c99aa35a5f9e81498d74a63b934a3e3
Manolo Bevia <mb863@cornell.edu>
2013-05-07 19:34:38 +0000

    Theming panels.
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@161 7fd527ee-9837-423d-921d-0a34619f9d71

--- js/panels-utils.js
+++ js/panels-utils.js
@@ -1,3 +1,56 @@
+
+(function($) {
+//Comment? get max height for panels
+
+var $header = $('.panels header');
+
+var headerHeight = $header.height() + parseInt($header.css("paddingTop") + $header.css("paddingBottom") + $header.css("marginTop") + $header.css("marginBottom"));
+
+var $carousel = $('.rs-carousel');
+
+var carouselHeight = $carousel.height() + parseInt($carousel.css("paddingTop") + $carousel.css("paddingBottom") + $carousel.css("marginTop") + $carousel.css("marginBottom"));
+
+var $winHeight = $(window).height();
+
+var maxHeightPanel = $winHeight - ((headerHeight + carouselHeight) + 10);
+  
+$('.panels-display').height(maxHeightPanel); 
+
+$('.panels-photo').height(maxHeightPanel);
+$('.panel-description').height(maxHeightPanel);
+$('.panels-display nav').height(maxHeightPanel);
+
+
+
+
+ 
+//console.log($('.win').html($winHeight));   
+          
+$(window).resize(function(){
+    
+    headerHeight = $header.height() + parseInt($header.css("paddingTop") + $header.css("paddingBottom") + $header.css("marginTop") + $header.css("marginBottom"));
+    
+    carouselHeight = $carousel.height() + parseInt($carousel.css("paddingTop") + $carousel.css("paddingBottom") + $carousel.css("marginTop") + $carousel.css("marginBottom"));
+
+    $winHeight = ($(window).height()) + 10;
+
+    maxHeightPanel = $winHeight - ((headerHeight + carouselHeight) + 10);
+    
+    $('.panels-display').height(maxHeightPanel);  
+    $('.panels-photo').height(maxHeightPanel);
+    $('.panel-description').height(maxHeightPanel);
+    $('.panels-display nav').height(maxHeightPanel);
+    
+    
+    //center panel
+    //$('#container').hide();
+    
+    $(':rs-carousel').carousel('refresh');
+    console.log( maxHeightPanel + " Refreshed");
+});
+
+
+
 $(document).ready(function () {
     $('.rs-carousel').carousel(
         {
@@ -12,6 +65,20 @@ $(document).ready(function () {
         }
     );
     
+    
+    //ZOOM
+    $('#container').tilezoom({
+			xml: 'dest/Panel79a.xml',
+			mousewheel: true,
+			navigation: true,
+			zoomIn: '#plus',
+			zoomOut: '#minus',
+			goHome: '#home',
+			navigation: '#panel-tools li.display-tools'
+			//'#panel-tools'
+
+		});
+		
 		
 		//hover over carousel of panels will change image legend to active color.
 		$('.panel-item').hover(function(){
@@ -21,6 +88,8 @@ $(document).ready(function () {
 		});
 
 });
+
+})(jQuery);
 
 
 
