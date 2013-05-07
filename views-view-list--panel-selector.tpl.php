ecb3b890bac17e8e6a295b69e6f383d8f134bf22
James Reidy <jgr25@cornell.edu>
2013-05-07 18:12:11 +0000

    unwind mods
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@158 7fd527ee-9837-423d-921d-0a34619f9d71

--- views-view-list--panel-selector.tpl.php
+++ views-view-list--panel-selector.tpl.php
@@ -16,36 +16,10 @@
   <?php if (!empty($title)) : ?>
     <h3><?php print $title; ?></h3>
   <?php endif; ?>
-  
-  <?php //print $list_type_prefix; ?>
-  
-  <nav class="rs-carousel">
-      <ul>
-  
-            
-                <?php foreach ($rows as $id => $row): ?>
-                    <li class="<?php print $classes_array[$id]; ?>">
-                    
-                    <?php print $row; ?>
-                    
-                        <p class="panel-item">
-                            <a href="#" title"">
-                                <img class="panel-image" src="<?php print $path_theme_panels; ?>/images/panels/carousel/Panel-B.jpg" width="148" height="198" alt="Panel B" />
-                                <span class="panel-name">Panel B</span>
-                            </a>                        
-                        </p>
-                    
-                    </li>
-                    
-                    
-                    
-                    
-                    
-                <?php endforeach; ?>
-    
-    </ul>
-  </nav>  
-  <?php //print $list_type_suffix; ?>
+  <?php print $list_type_prefix; ?>
+    <?php foreach ($rows as $id => $row): ?>
+      <li class="<?php print $classes_array[$id]; ?>"><?php print $row; ?></li>
+    <?php endforeach; ?>
+  <?php print $list_type_suffix; ?>
 <?php print $wrapper_suffix; ?>
-
 
