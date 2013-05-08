429de3fd6e6f7ed67d49e8b4cbb361b56009fb50
James Reidy <jgr25@cornell.edu>
2013-05-08 19:37:45 +0000

    move css and js into theme handler for  carousel
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@163 7fd527ee-9837-423d-921d-0a34619f9d71

--- views-view-list--panel-selector.tpl.php
+++ views-view-list--panel-selector.tpl.php
@@ -12,6 +12,23 @@
  * @ingroup views_templates
  */
 ?>
+<?php
+if (!empty($rows)) {
+  // add css to suport carousel
+  drupal_add_css(drupal_get_path('theme', 'warburg') . 'css/content.css', array('type' => 'file'));
+  drupal_add_css(drupal_get_path('theme', 'warburg') . 'css/panels.css', array('type' => 'file'));
+  drupal_add_css(drupal_get_path('theme', 'warburg') . 'css/panels/carousel/jquery.rs.carousel.css', array('type' => 'file'));
+
+  // add javascript to support carousel
+  drupal_add_js(drupal_get_path('theme', 'warburg') . 'js/carousel/lib/jquery.ui.widget.js', array('group' => JS_THEME));
+  drupal_add_js(drupal_get_path('theme', 'warburg') . 'js/carousel/lib/jquery.event.drag.js', array('group' => JS_THEME));
+  drupal_add_js(drupal_get_path('theme', 'warburg') . 'js/carousel/lib/jquery.translate3d.js', array('group' => JS_THEME));
+  drupal_add_js(drupal_get_path('theme', 'warburg') . 'js/carousel/jquery.rs.carousel.js', array('group' => JS_THEME));
+  drupal_add_js(drupal_get_path('theme', 'warburg') . 'js/carousel/jquery.rs.carousel-autoscroll.js', array('group' => JS_THEME));
+  drupal_add_js(drupal_get_path('theme', 'warburg') . 'js/carousel/jquery.rs.carousel-continuous.js', array('group' => JS_THEME));
+  drupal_add_js(drupal_get_path('theme', 'warburg') . 'js/carousel/jquery.rs.carousel-touch.js', array('group' => JS_THEME));
+}
+?>
 <?php print $wrapper_prefix; ?>
   <?php if (!empty($title)) : ?>
     <h3><?php print $title; ?></h3>
