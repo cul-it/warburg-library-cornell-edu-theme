8bc82fef191be7414f51ba429dba3479ae7860fc
Melissa Wallace <mhk33@cornell.edu>
2013-05-13 14:55:14 +0000

    add home page content blocks
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@247 7fd527ee-9837-423d-921d-0a34619f9d71

--- page--front.tpl.php
+++ page--front.tpl.php
@@ -103,15 +103,16 @@
 
 
 <section class="hero">
- <section class="content"> 
-    <h2>Sunt in culpa qui officia.</h2>
+ <section class="content">
+    <?php print render($page['intro_text']); ?>
+    <!-- <h2>Sunt in culpa qui officia.</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> 
     <nav>
       <ul>
         <li><a class="btn-pathway" href="#"><i class="icon-pathways"></i> Guided Pathways</a></li>
         <li><a class="btn-panels" href="#"><i class="icon-panels"></i> All panels</a></li>
       <ul>
-    </nav>
+    </nav> -->
   </section>
   <aside>
     <a href="#"><img class="book-cover" src="/sites/all/themes/warburg/images/johnson-cover.jpg" /></a>
