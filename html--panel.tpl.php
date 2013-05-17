a3601e53aba1a2733ed18794a44afa64aa39dee6
James Reidy <jgr25@cornell.edu>
2013-05-17 19:00:46 +0000

    just one version of jquery
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@347 7fd527ee-9837-423d-921d-0a34619f9d71

--- html--panel.tpl.php
+++ html--panel.tpl.php
@@ -183,7 +183,7 @@
 
     </div>
 
-    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <!-- <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script> -->
     <!--<script src="js/bootstrap.min.js"></script>-->
 
     <!-- carousel lib -->
