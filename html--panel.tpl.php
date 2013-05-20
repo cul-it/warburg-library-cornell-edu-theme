be4446caefcbe6815a685e1e284250d65de827c7
James Reidy <jgr25@cornell.edu>
2013-05-20 19:50:54 +0000

    find a
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@400 7fd527ee-9837-423d-921d-0a34619f9d71

--- html--panel.tpl.php
+++ html--panel.tpl.php
@@ -97,7 +97,7 @@
             zoomOut: '#minus',
             goHome: '#home',
             beforeZoom: function($cont) {
-              active = $cont.data('tilezoom.settings').hotspots.find('[href]');
+              active = $cont.data('tilezoom.settings').hotspots.find('a');
               alert(active.toSource());
               }
           });
