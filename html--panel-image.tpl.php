16940274fef35eb9387408a796b71c1ef255e040
James Reidy <jgr25@cornell.edu>
2013-05-20 21:19:42 +0000

    leave off ;
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@416 7fd527ee-9837-423d-921d-0a34619f9d71

--- html--panel-image.tpl.php
+++ html--panel-image.tpl.php
@@ -204,10 +204,10 @@
                   <li><a class="original active" href="<?php print $overview_link; ?>" title=""></a></li>
                   <?php if (!empty($images_link)): ?>
                   <li><a class="map" href="<?php $images_link; ?>" title=""></a></li>
-                  <?php endif; ?>
+                  <?php endif ?>
                   <?php if (!empty($pathways_link)): ?>
                   <li><a class="pathway" href="<?php $pathways_link; ?>" title=""></a></li>
-                  <?php endif; ?>
+                  <?php endif ?>
 
                   <li class="display-tools"></li>
                 </ul>
