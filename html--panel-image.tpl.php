747f1afe2c2e7240563ff965010b5036561d9e49
James Reidy <jgr25@cornell.edu>
2013-05-20 21:12:44 +0000

    don't show buttons that have no links
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@413 7fd527ee-9837-423d-921d-0a34619f9d71

--- html--panel-image.tpl.php
+++ html--panel-image.tpl.php
@@ -197,13 +197,17 @@
 
                   <?php
                   $overview_link = $variables['warburg']['nav']['overview'];
-                  $images_link = $variables['warburg']['nav']['images'];
-                  $pathways_link = $variables['warburg']['nav']['pathways'];
+                  $images_link = empty($variables['warburg']['nav']['images']) ? '' : $variables['warburg']['nav']['images'];
+                  $pathways_link = empty($variables['warburg']['nav']['pathways']) ? '' : $variables['warburg']['nav']['pathways'];
                   ?>
 
                   <li><a class="original active" href="<?php print $overview_link; ?>" title=""></a></li>
+                  <?php if (!empty($images_link): )
                   <li><a class="map" href="<?php $images_link; ?>" title=""></a></li>
+                  <?php endif; ?>
+                  <?php if (!empty($pathways_link)): ?>
                   <li><a class="pathway" href="<?php $pathways_link; ?>" title=""></a></li>
+                  <?php endif; ?>
 
                   <li class="display-tools"></li>
                 </ul>
