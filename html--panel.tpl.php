826cffff24fde818aac2bb7ad8f8bdbf9d2c0972
Manolo Bevia <mb863@cornell.edu>
2013-05-21 15:46:41 +0000

    Making search responsive.
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@433 7fd527ee-9837-423d-921d-0a34619f9d71

--- html--panel.tpl.php
+++ html--panel.tpl.php
@@ -104,7 +104,7 @@
                 ?>
             </nav>
 
-            <a class="search-panels" href="#" title=""><span class="search-panels-icon"></span></a>
+            <a class="search-panels" href="#" title="Search panels"><span class="search-panels-icon"></span></a>
 
              <form id="search-panels" action="#" method="get" name="search-panels">
                     <input name="query" type="text" value="" />
