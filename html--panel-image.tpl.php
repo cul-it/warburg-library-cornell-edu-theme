49ef5f5fd3de06c454d170c43252a667fa1966ac
James Reidy <jgr25@cornell.edu>
2013-05-23 14:13:50 +0000

    change block id
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@467 7fd527ee-9837-423d-921d-0a34619f9d71

--- html--panel-image.tpl.php
+++ html--panel-image.tpl.php
@@ -147,7 +147,7 @@
 
             <section class="panel-description">
                     <?php
-                    $result = module_invoke('views', 'block_view', 'panel_image_description_block-block');
+                    $result = module_invoke('views', 'block_view', '240f2561056d0722cec3e68a281bd9e6');
                     print render($result);
                     ?>
 
