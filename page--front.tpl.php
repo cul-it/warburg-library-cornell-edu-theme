c79e2909ec579f35bf4f7beb08bac067e2ca9baf
Jenn Colt <jrc88@cornell.edu>
2013-05-29 14:37:02 +0000

    Type commit message here…
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@493 7fd527ee-9837-423d-921d-0a34619f9d71

--- page--front.tpl.php
+++ page--front.tpl.php
@@ -73,7 +73,6 @@
 <section class="institution-brand">
   <div class="container">
     <a href="http://www.library.cornell.edu">Cornell University Library</a> | <a href="http://warburg.sas.ac.uk/home/">The Warburg Institute</a> | <a href="http://www.cornellpress.cornell.edu/">Cornell University Press</a>
-  </div>
     <a class="search-panels" href="#" title="Search panels"><span class="search-panels-icon"></span></a>
     <form id="search-panels" action="#" method="get" name="search-panels">
       <input name="query" type="text" value="" />
