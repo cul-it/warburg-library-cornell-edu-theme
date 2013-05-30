7a555b66aa95d738df5aaa3dd3fbaa80327520f9
Manolo Bevia <mb863@cornell.edu>
2013-05-30 17:41:45 +0000

    /Wrapping all in an anonymous self invoking function because Drupal is too delicate.
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@525 7fd527ee-9837-423d-921d-0a34619f9d71

--- js/content-utils.js
+++ js/content-utils.js
@@ -1,23 +1,21 @@
 // It will handle user interaction just for content, not panels. For example: revealing phonetics, search form, etc.
 
-$(document).ready(function () {
-     
-  		
-		   var $searchIcon = $('.search-panels');
-		   var $searchIconImg = $('.search-panels span');
-		   var $searchFlag = true;
-  			$searchIcon .click(function(){
-  			  
-  			  if ($searchFlag) {
-  			    $('#search-panels').show();
-  			      $searchIconImg.removeClass("search-panels-icon");
-  			      $searchIconImg.addClass("search-panels-close-icon");
-  			      $searchFlag = !$searchFlag;
-  			  }else{
-            $('#search-panels').hide();
-              $searchIconImg.addClass("search-panels-icon");
-              $searchIconImg.removeClass("search-panels-close-icon");
-              $searchFlag = !$searchFlag;
-  			  };
-  		
-  			});
+//Wrapping all in an anonymous self invoking function because Drupal is too delicate.
+
+(function ($) {
+    var $searchIcon = $('.search-panels');
+    var $searchIconImg = $('.search-panels span');
+    var $searchFlag = true;
+    $searchIcon .click(function(){
+      if ($searchFlag) {
+        $('#search-panels').show();
+        $searchIconImg.removeClass("search-panels-icon");
+        $searchIconImg.addClass("search-panels-close-icon");
+        $searchFlag = !$searchFlag;
+      }else{
+        $('#search-panels').hide();
+        $searchIconImg.addClass("search-panels-icon");
+        $searchIconImg.removeClass("search-panels-close-icon");
+        $searchFlag = !$searchFlag;
+      };
+})(jQuery);
