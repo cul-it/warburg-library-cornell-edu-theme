fd9d8dcc5ab7cca757b529d3e178c45efc1a6240
James Reidy <jgr25@cornell.edu>
2013-06-13 22:14:58 +0000

    image/sequence tools ever-present but with enabled/disabled classes
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@577 7fd527ee-9837-423d-921d-0a34619f9d71

--- html--panel.tpl.php
+++ html--panel.tpl.php
@@ -198,18 +198,22 @@
             </section>
 
             <nav>
-               <ul id="panel-tools">
+              <ul id="panel-tools">
 
-                   <li><a class="original active" href="<?php print $variables['warburg']['nav']['overview']; ?>" title="full panel"></a></li>
-                  <?php if (!empty($variables['warburg']['nav']['images'])): ?>
-                  <li><a class="map" href="<?php print $variables['warburg']['nav']['images']; ?>" title="individual images"></a></li>
-                  <?php endif ?>
-                  <?php if (!empty($variables['warburg']['nav']['pathways'])): ?>
-                  <li><a class="pathway" href="<?php print $variables['warburg']['nav']['pathways']; ?>" title="guided pathways"></a></li>
-                  <?php endif ?>
+                <li><a class="original active" href="<?php print $variables['warburg']['nav']['overview']; ?>" title="full panel"></a></li>
+                <?php
+                $nav_classes = $variables['warburg']['nav']['images_classes'];
+                $nav_link = empty($variables['warburg']['nav']['images']) ? '#' : empty($variables['warburg']['nav']['images']);
+                ?>
+                <li><a class="map <?php print $nav_classes; ?>" href="<?php print $nav_link; ?>" title="individual images"></a></li>
+                <?php
+                $nav_classes = $variables['warburg']['nav']['pathways_classes'];
+                $nav_link = empty($variables['warburg']['nav']['pathways']) ? '#' : empty($variables['warburg']['nav']['pathways']);
+                ?>
+                <li><a class="pathway <?php print $nav_classes; ?>" href="<?php print $nav_link; ?>" title="guided pathways"></a></li>
 
-                  <li class="display-tools"></li>
-                </ul>
+                <li class="display-tools"></li>
+              </ul>
             </nav>
 
 
