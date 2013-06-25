a8dd2568bfd00894a4ad1e01e655b39f07106b1a
Manolo Bevia <mb863@cornell.edu>
2013-06-25 14:32:40 +0000

    change title tag for search.
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@683 7fd527ee-9837-423d-921d-0a34619f9d71

--- page.tpl.php
+++ page.tpl.php
@@ -74,7 +74,7 @@
   <div class="container">
     <a href="http://www.library.cornell.edu">Cornell University Library</a> | <a href="http://warburg.sas.ac.uk/home/">The Warburg Institute</a> |      <a href="http://www.cornellpress.cornell.edu/">Cornell University Press</a>
 
-    <a class="search-panels" href="#" title="Search"><span class="search-panels-icon"></span></a>
+    <a class="search-panels" href="#" title="search"><span class="search-panels-icon"></span></a>
 
     <form id="search-panels" name="search-panels" action="/search/node" method="post" enctype="application/x-www-form-urlencoded" accept-charset="UTF-8">
             <input name="keys" type="text" value="" />
