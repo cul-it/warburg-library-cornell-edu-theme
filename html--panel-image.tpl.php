940de2f8bf6130ef721366c6809521130cf27036
James Reidy <jgr25@cornell.edu>
2013-07-10 18:50:20 +0000

    ajax support add spot_metadata_link
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@860 7fd527ee-9837-423d-921d-0a34619f9d71

--- html--panel-image.tpl.php
+++ html--panel-image.tpl.php
@@ -119,7 +119,7 @@
                         if (!empty($variables['warburg']['ordinal'])) {
                           print '<div class="zoom-hotspots">' . PHP_EOL;
                           foreach ($variables['warburg']['ordinal'] as $hotspot) {
-                            print $hotspot['spot_link'] . PHP_EOL;
+                            print $hotspot['spot_metadata_link'] . PHP_EOL;
                           }
                           print '</div>' . PHP_EOL;
                         }
