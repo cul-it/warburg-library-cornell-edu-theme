1c0cb5093d9a0ffa3dcf5d55197a17700effdfd2
James Reidy <jgr25@cornell.edu>
2013-07-12 15:57:54 +0000

    try jquery dialog()
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@863 7fd527ee-9837-423d-921d-0a34619f9d71

--- js/draggable-modal.js
+++ js/draggable-modal.js
@@ -1,5 +1,9 @@
 (function($) {
-	$("#additionalImage").draggable({
-	    handle: ".modal-header"
-	});
+	//$("#additionalImage").draggable({
+	//    handle: ".modal-header"
+	//});
+  $("#additionalImage").dialog({
+    height:140,
+    model: true
+  });
 });
