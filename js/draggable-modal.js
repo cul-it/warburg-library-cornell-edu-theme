6d8879ba60deee5744d42928e217c3887ce4bbc1
James Reidy <jgr25@cornell.edu>
2013-07-12 17:42:31 +0000

    try a different draggable
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@873 7fd527ee-9837-423d-921d-0a34619f9d71

--- js/draggable-modal.js
+++ js/draggable-modal.js
@@ -1,13 +1,17 @@
 (function ($) {
- (function($) {
-  	$("#additionalImage").draggable({
-  	    handle: ".modal-header"
-  	});
-  });
+ // (function($) {
+ //  	$("#additionalImage").draggable({
+ //  	    handle: ".modal-header"
+ //  	});
+ //  });
   // $('body').on('click', '#additional-image-link', function() {
   //   alert('clicked on #additional-image-link');
   //   //$("#additionalImage").dialog('open');
   //   $("#test-dialog").dialog('open');
   //   return false;
   // });
+  $('#additionalImage').modal({onShow: function (dialog) {
+  dialog.container.draggable();
+  }});
+
 })(jQuery);
