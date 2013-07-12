98d13346a2a67c7bd04029800f9f528ad6b8643d
James Reidy <jgr25@cornell.edu>
2013-07-12 19:01:04 +0000

    go back to click handler
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@888 7fd527ee-9837-423d-921d-0a34619f9d71

--- js/draggable-modal.js
+++ js/draggable-modal.js
@@ -1,6 +1,5 @@
 (function ($) {
-  // $('body').on('click', "#additional-image-link", function() {
-  //   $('#additionalImage').modal('show').draggable();
-  // });
-  $('#additionalImage').modal().draggable();
+  $('body').on('click', "#additional-image-link", function() {
+    $('#additionalImage').modal('show').draggable();
+  });
 })(jQuery);
