260e76fe6647e9276a1ee62cd85ddcb8fc6be5fd
James Reidy <jgr25@cornell.edu>
2013-07-12 16:39:30 +0000

    add function
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@865 7fd527ee-9837-423d-921d-0a34619f9d71

--- js/draggable-modal.js
+++ js/draggable-modal.js
@@ -1,9 +1,13 @@
-(function($) {
-	//$("#additionalImage").draggable({
-	//    handle: ".modal-header"
-	//});
-  $("#additionalImage").dialog({
-    height:140,
-    model: true
+(function ($) {
+  (function($) {
+  	//$("#additionalImage").draggable({
+  	//    handle: ".modal-header"
+  	//});
+    function showAdditionalImage() {
+      $("#additionalImage").dialog({
+        height:140,
+        model: true
+      });
+    }
   });
-});
+})(jQuery);
