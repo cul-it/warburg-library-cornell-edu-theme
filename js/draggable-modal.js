e613ce9aa0ded34a19f21e1011afb77dc00c6c46
James Reidy <jgr25@cornell.edu>
2013-07-12 18:51:40 +0000

    remove commented out stuff
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@886 7fd527ee-9837-423d-921d-0a34619f9d71

--- js/draggable-modal.js
+++ js/draggable-modal.js
@@ -1,28 +1,5 @@
 (function ($) {
- // (function($) {
- //  	$("#additionalImage").draggable({
- //  	    handle: ".modal-header"
- //  	});
- //  });
-  // $('body').on('click', '#additional-image-link', function() {
-  //   alert('clicked on #additional-image-link');
-  //   //$("#additionalImage").dialog('open');
-  //   $("#test-dialog").dialog('open');
-  //   return false;
-  // });
-  // $('#additionalImage').modal({onShow: function (dialog) {
-  // dialog.container.draggable();
-  // }});
-  // $('body').on('show', '#additionalImage', function(dialog) {
-  //   dialog.container.draggable();
-  // });
-
   $('body').on('click', "#additional-image-link", function() {
-    // $('#additionalImage').modal({
-    //   show: true,
-    //   onShow: function (dialog) {
-    //     dialog.container.draggable()
-    //   }});
     $('#additionalImage').modal('show').draggable();
   });
 })(jQuery);
