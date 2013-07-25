db4ec3dda8f8f4585cd6c7639f95033f036f2066
James Reidy <jgr25@cornell.edu>
2013-07-25 20:10:08 +0000

    make multiple context images draggable
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@963 7fd527ee-9837-423d-921d-0a34619f9d71

--- js/draggable-modal.js
+++ js/draggable-modal.js
@@ -1,5 +1,17 @@
 (function ($) {
-  $('body').on('click', "#additional-image-link", function() {
-    $('#additionalImage').modal('show').draggable();
+  $('body').on('click', "#additional-image-link-0", function() {
+    $('#additionalImage-0').modal('show').draggable();
+  });
+  $('body').on('click', "#additional-image-link-1", function() {
+    $('#additionalImage-1').modal('show').draggable();
+  });
+  $('body').on('click', "#additional-image-link-2", function() {
+    $('#additionalImage-2').modal('show').draggable();
+  });
+  $('body').on('click', "#additional-image-link-3", function() {
+    $('#additionalImage-3').modal('show').draggable();
+  });
+  $('body').on('click', "#additional-image-link-4", function() {
+    $('#additionalImage-4').modal('show').draggable();
   });
 })(jQuery);
