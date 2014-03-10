560fba5b3cfeaa968e6681d0f7e521105a23b89b
James Reidy <jgr25@cornell.edu>
2014-03-10 19:03:52 +0000

    remove debugging
    git-svn-id: https://svn.library.cornell.edu/warburg/trunk@1129 7fd527ee-9837-423d-921d-0a34619f9d71

--- html--panel.tpl.php
+++ html--panel.tpl.php
@@ -101,13 +101,11 @@
         <section>
 				<a title="collapse panels" class="carousel-toggle carousel-toggle-hide" href="#"></a>
             <?php
-            print 'jgr25 html--panel.tpl.php';
             if (isset($_GET['theme'])) {
               $theme_term = $_GET['theme'];
               $selector = 'panel_selector-block_1';
             }
             else if (isset($_GET['view']) && ($_GET['view'] == 'pathways')) {
-              print 'pathways found';
               $theme_term = 'all';
               $selector = 'panel_selector-block_2';
             }
